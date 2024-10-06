b1389a588e4e7f3bf0c2d028436e1f460bb97629
Ruben <1511ruben@gmail.com>
2024-10-05 23:14:49 -0600

    Education

---
+++ gomitas_g/app/education/page.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const plans=[
+    {
+        name:"Elementary School",
+        description:"",
+        price:15
+    },    {
+        name:"High School",
+        description:"",
+        price:30
+    },    {
+        name:"University",
+        description:"",
+        price:45
+    }
+]
+
+export default function page() {
+  return (
+    <div className="flex flex-col">
+      <div className="">
+        <h1 className="text-[40px] font-bold">Pricing</h1>
+      </div>
+      <div className="">
+        <div className="flex flex-row">
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
