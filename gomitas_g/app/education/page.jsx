d6f7c3b0228dfb503b61393004974ab24a74c247
BryanSigala <82849413+BryanSigala@users.noreply.github.com>
2024-10-06 07:38:25 -0600

    Add Cards components to Educate page.jsx

--- gomitas_g/app/education/page.jsx
+++ gomitas_g/app/education/page.jsx
@@ -2,8 +2,31 @@
 
 import React from 'react'
 import Image from 'next/image';
+import EducateCard from '../components/ui/EducateCard';
 
 const EducationPage = () => {
+
+  const data = [
+    {
+      grade: "Elementary School", 
+      benefits: [
+        "Benefit 1", "Benefit 2", "Benefit 3",
+      ]
+    },
+    {
+      grade: "High School", 
+      benefits: [
+        "Benefit 1", "Benefit 2", "Benefit 3",
+      ]
+    },
+    {
+      grade: "University", 
+      benefits: [
+        "Benefit 1", "Benefit 2", "Benefit 3",
+      ]
+    },
+  ]
+
   return (
     <div className="relative min-h-screen">
       {/* Fondo animado */}
@@ -25,8 +48,14 @@ const EducationPage = () => {
       {/* Contenido principal */}
       <div className="relative z-20 align-items-center justify-center">
         {/* Aquí va todo el contenido de tu página */}
-        <div className="">
-          Card
+        <div className="flex flex-wrap justify-center gap-6 py-10">
+          {data.map((card, index) => (
+            <EducateCard
+              key={index}
+              grade={card.grade}
+              benefits={card.benefits}
+            />
+          ))}
         </div>
       </div>
     </div>
