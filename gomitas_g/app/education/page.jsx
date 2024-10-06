b73d3db984903796a303096ee9bb5bc1870f0ec2
BryanSigala <82849413+BryanSigala@users.noreply.github.com>
2024-10-06 04:45:07 -0600

    Add BG image to page.jsx/education

--- gomitas_g/app/education/page.jsx
+++ gomitas_g/app/education/page.jsx
@@ -1,34 +1,36 @@
+"use client";
+
 import React from 'react'
+import Image from 'next/image';
 
-const plans=[
-    {
-        name:"Elementary School",
-        benefits:[
-          "Benefit 1", 
-          "Benefit 2", 
-          "Benefit 3",
-        ],
-    },    {
-        name:"High School",
-        benefits:[
-          "Benefit 1", 
-          "Benefit 2", 
-          "Benefit 3",
-        ],
-    },    {
-        name:"University",
-        benefits:[
-          "Benefit 1", 
-          "Benefit 2", 
-          "Benefit 3",
-        ],
-    }
-]
-
-export default function page() {
+const EducationPage = () => {
   return (
-    <div>
-      Hola
+    <div className="relative min-h-screen">
+      {/* Fondo animado */}
+      <div className="absolute inset-0 z-0">
+        {/* Aquí va tu fondo animado */}
+      </div>
+
+      {/* Imagen de fondo */}
+      <div className="absolute inset-0 z-10">
+        <Image 
+          src="/Education-BG.jpg"
+          alt=""
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
+
+      {/* Contenido principal */}
+      <div className="relative z-20 align-items-center justify-center">
+        {/* Aquí va todo el contenido de tu página */}
+        <div className="">
+          Card
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default EducationPage;
